ecf3d472acc9e53230edc242c2ab062f3e2fb03b
hamadou diallo <hamadou_diallo@yahoo.com>
2024-01-19 10:46:52 -0500

    Filter function added depending on requirement, we also made use of forwardRef to get our ref and reuse DropDown component for cleaner, efficient code

--- src/utilities/functions.js
+++ src/utilities/functions.js
@@ -22,3 +22,52 @@ export const getAggregateTotal = (group, datas) => {
   });
   return groupTotal;
 };
+
+export const getAggregateTotalByFilterRequirements = (
+  groups,
+  filterOptions,
+  datas
+) => {
+  let groupTotal = Array(groups.length).fill(0);
+  const { homeOwnership, year, quarter, term } = filterOptions;
+  let allFilterList = [];
+  groups.forEach((group) => {
+    const filterResult = datas.filter((data) => {
+      if (group === data.grade) {
+        if (homeOwnership || year || quarter || term) {
+          return (
+            (homeOwnership ? homeOwnership === data.homeOwnership : true) &&
+            (year ? year === data.year : true) &&
+            (quarter ? quarter === data.quarter : true) &&
+            (term ? term === data.term : true)
+          );
+        }
+      }
+    });
+    allFilterList = [...allFilterList, ...filterResult];
+  });
+  allFilterList.forEach((list) => {
+    groupTotal[+list.grade - 1] += +list.currentBalance;
+  });
+  return groupTotal;
+};
+
+export const getAllGroups = (datas) => {
+  let group = {};
+  datas.forEach((data) => {
+    for (const key in data) {
+      if (key !== "currentBalance" && key !== "grade") {
+        if (!group[key]) {
+          group[key] = new Set();
+        }
+        if (data[key]) {
+          group[key].add(data[key]);
+        }
+      }
+    }
+  });
+  for (const key in group) {
+    group[key] = [...group[key]].sort();
+  }
+  return group;
+};
